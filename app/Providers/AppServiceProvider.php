baac6fbb247bc600cfde6d4418c6d6ed039b6dbc
Richard Trujillo <richard@desemax.com>
2018-09-14 19:29:54 +0200

    Add custom env blade directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::if('isProduction', function () {
+            $env = env('APP_ENV', 'production');
+
+            return (boolean) ($env === 'production');
+        });
+
+        Blade::if('isLocal', function () {
+            $env = env('APP_ENV', 'production');
+
+            return (boolean) ($env === 'local');
+        });
     }
 
     /**
