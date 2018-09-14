3e35f3def78a52c0270a31d53a67c15c7a547c4c
Richard Trujillo <richard@desemax.com>
2018-09-14 20:03:54 +0200

    Add local env helper function

--- app/helpers.php
+++ app/helpers.php
@@ -8,3 +8,12 @@ if (! function_exists('isProduction')) {
         return (boolean) ($env === 'production');
     }
 }
+
+if (! function_exists('isLocal')) {
+    function isLocal()
+    {
+        $env = env('APP_ENV', 'production');
+
+        return (boolean) ($env === 'local');
+    }
+}
